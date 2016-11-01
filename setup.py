9961d9ade6b84d34d524466ddd8bc479f3b3e4bd
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-11-01 10:22:21 -0400

    Require `matplotlib` as a dependency.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    # TODO: put package requirements here
+    "matplotlib",
 ]
 
 test_requirements = [
